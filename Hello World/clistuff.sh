241dc905bc541c076b3032fd185050e99fa47b82
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-07-18 22:26:43 -0400

    updated clistuff.sh

--- Hello World/clistuff.sh
+++ Hello World/clistuff.sh
@@ -1,4 +1,4 @@
-#For Josh: thsi is a simple Hello World! project build with maven. Make sure you have maven installed and added to your class path!! Chmod it and run it in an empty folder.
+#For Josh: this is a simple Hello World! project build with maven. Make sure you have maven installed and added to your class path!! Chmod it and run it in an empty folder.
 
 mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
 cd my-app/

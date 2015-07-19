89c7004cefb93b1c2421421b148f44f9b80e2de7
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-07-18 22:24:57 -0400

    added archetype generation test

---
+++ Hello World/clistuff.sh
@@ -0,0 +1,6 @@
+#For Josh: thsi is a simple Hello World! project build with maven. Make sure you have maven installed and added to your class path!! Chmod it and run it in an empty folder.
+
+mvn archetype:generate -DgroupId=com.mycompany.app -DartifactId=my-app -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+cd my-app/
+mvn package
+java -cp target/my-app-1.0-SNAPSHOT.jar com.mycompany.app.App
